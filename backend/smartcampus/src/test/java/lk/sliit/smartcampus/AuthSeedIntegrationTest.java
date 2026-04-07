01a75783a8e354005533f4a5a45c64686d1c00d3
Kasunianupama <kasunianupama509@gmail.com>
2026-04-07 23:15:19 +0530

    fix(tests): force H2 database replacement in AuthSeedIntegrationTest

--- backend/smartcampus/src/test/java/lk/sliit/smartcampus/AuthSeedIntegrationTest.java
+++ backend/smartcampus/src/test/java/lk/sliit/smartcampus/AuthSeedIntegrationTest.java
@@ -4,10 +4,25 @@ import lk.sliit.smartcampus.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+// NEW SPRING BOOT 4.0 IMPORT:
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+// THE ULTIMATE SHIELD: Forcefully kills MySQL connections and uses H2 for this test
+@ActiveProfiles("test") 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY) 
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=MySQL",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.flyway.enabled=false",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class AuthSeedIntegrationTest {
 
     @Autowired
