b93f222e2aa730c8bf356adf63c6084453f20744
Dhanoshi24 <dhanoshi9924@gmail.com>
2026-04-23 11:44:29 +0530

    update signup request

--- backend/smartcampus/src/main/java/lk/sliit/smartcampus/auth/dto/SignupRequest.java
+++ backend/smartcampus/src/main/java/lk/sliit/smartcampus/auth/dto/SignupRequest.java
@@ -2,7 +2,6 @@ package lk.sliit.smartcampus.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public class SignupRequest {
 
